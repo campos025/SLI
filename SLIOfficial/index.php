5c5e881382d6eba14ff26e377f08b806569f994f
RijanLexterr <94050658+RijanLexterr@users.noreply.github.com>
2024-11-13 13:32:22 +0800

    Update index.php

--- SLIOfficial/index.php
+++ SLIOfficial/index.php
@@ -401,72 +401,15 @@
         <!-- Grid row -->
         <div class="row mt-3">
           <!-- Grid column -->
-          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-            <!-- Content -->
-            <h6 class="text-uppercase fw-bold mb-4">
-              <i class="fas fa-gem me-3"></i>SALT AND LIGHT
-
-            </h6>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua
-            </p>
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold mb-4">
-              Products
-            </h6>
-            <p>
-              <a href="#!" class="text-reset">Angular</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">React</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Vue</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Laravel</a>
-            </p>
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold mb-4">
-              Useful links
-            </h6>
-            <p>
-              <a href="#!" class="text-reset">Pricing</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Settings</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Orders</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Help</a>
-            </p>
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
           <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
             <!-- Links -->
             <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
-            <p><i class="fas fa-home me-3"></i> Salt and Light Faith Center, Blk. 4, Lot 5 & 7, Elvinda 7, Barangay
+            <p><i class="fas fa-home me-3"></i>Salt and Light Faith Center, Blk. 4, Lot 5 & 7, Elvinda 7, Barangay
               Cuyab, San Pedro, Philippines, 4023</p>
             <p>
-              <i class="fas fa-envelope me-3"></i>
-              saltandlight2009@gmail.com
+              <i class="fas fa-envelope me-3"></i>saltandlight2009@gmail.com
             </p>
-            <p><i class="fas fa-phone me-3"></i> + 288240863</p>
+            <p><i class="fas fa-phone me-3"></i>+288240863</p>
 
           </div>
           <!-- Grid column -->
