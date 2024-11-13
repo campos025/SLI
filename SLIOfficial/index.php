66d252cb8ae823f4b4bc3edc022817a34c124a48
RijanLexterr <94050658+RijanLexterr@users.noreply.github.com>
2024-11-13 13:34:45 +0800

    Update index.php

--- SLIOfficial/index.php
+++ SLIOfficial/index.php
@@ -420,7 +420,7 @@
     <!-- Section: Links  -->
 
     <!-- Copyright -->
-    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    <div class="p-4" style="background-color: rgba(0, 0, 0, 0.05);">
       © 2024 Copyright:
       <a class="text-reset fw-bold" href="">SALT AND LIGHT FELLOWSHIP</a>
     </div>
